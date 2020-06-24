19379b390fef3c32e03d01fb098e8245c76d357d
VisharadVerma01 <visharadverma@gmail.com>
2020-06-24 23:11:21 +0530

    Allowed Hosts

--- learningusers/learningusers/settings.py
+++ learningusers/learningusers/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '=s+8hassvp9gn+x(lis)r)_4s9$4uyq+u4ul)2vfuay$u%@9u#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
